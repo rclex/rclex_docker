91085a59accf6c17c031dd705e7b44ed11803294
Ryota Kinukawa <kinukawa@tombo-works.com>
2022-07-04 19:29:38 +0900

    add git_hooks config

---
+++ config/config.exs
@@ -0,0 +1,15 @@
+import Config
+
+if config_env() == :dev do
+  config :git_hooks,
+    auto_install: true,
+    verbose: true,
+    hooks: [
+      pre_commit: [
+        tasks: [
+          {:cmd, "mix test"},
+          {:cmd, "mix format --check-formatted"}
+        ]
+      ]
+    ]
+end
